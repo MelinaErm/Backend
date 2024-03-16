5e546787bbdd40aa7e2a5efe10baff3b32f9af66
Melina <melinaermeidi@yahoo.gr>
2024-03-17 00:28:52 +0200

    commit no.10

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 console.log('this is a test no.3');
-const express = require('express')
+const express = require('express') //express 
 const app = express()
+
+//declare a route
+//get route
+
+app.get('/',(req,res)=>{
+
+    res.send('hello node API')
+
+})
+
 
 app.listen(3000,()=>{
     console.log('node API app is running on port 3000')
