b203ad93e80ae5ae95a36040550a1914c5401648
Melina <melinaermeidi@yahoo.gr>
2024-03-18 21:32:18 +0200

    commits

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ console.log('this is a test no.3');
 const express = require('express') //express 
 const app = express()
 
+const mongoose = require('mongoose') //mongoose
+
+
 //declare a route
 //get route
-
 app.get('/',(req,res)=>{
     res.send('hello node API')
 
@@ -16,4 +18,14 @@ app.get('/blog',(req,res)=>{
 
 app.listen(3000,()=>{
     console.log('node API app is running on port 3000')
+})
+
+
+//mongoose connection (mongo db)
+
+mongoose.connect('mongodb+srv://admin:12!56!79@devapi.arzcgkl.mongodb.net/Node-API?retryWrites=true&w=majority&appName=DevAPI')
+.then(()=>{
+    console.log('connected to MongoDB')
+}).catch((error)=>{
+    console.log(error)
 })
