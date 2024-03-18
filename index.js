3c55aea2976478ca4bcccd5712c9783ba1dd1ffc
Melina <melinaermeidi@yahoo.gr>
2024-03-18 21:48:10 +0200

    commit

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ app.get('/blog',(req,res)=>{
     res.send('Hello Blog')
 })
 
-app.listen(3000,()=>{
-    console.log('node API app is running on port 3000')
-})
-
 
 //mongoose connection (mongo db)
 
 mongoose.connect('mongodb+srv://admin:12!56!79@devapi.arzcgkl.mongodb.net/Node-API?retryWrites=true&w=majority&appName=DevAPI')
 .then(()=>{
+    app.listen(3000,()=>{
+        console.log('node API app is running on port 3000')
+    })
+    
     console.log('connected to MongoDB')
 }).catch((error)=>{
     console.log(error)
